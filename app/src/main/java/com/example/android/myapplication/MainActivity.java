3120f8d75fe321837efd68c003f2601a66e2262b
Rob Hess <robwhess@gmail.com>
2017-05-09 22:08:02 -0700

    Add scratch code from 5/9 lecture.

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.myapplication;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String LIFECYCLE_EVENTS_KEY = "lifecycleEvents";
 
     private TextView mLifecycleEventsTV;
 
@@ -18,12 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
         mLifecycleEventsTV = (TextView)findViewById(R.id.tv_lifecycle_events);
 
+        if (savedInstanceState != null && savedInstanceState.containsKey(LIFECYCLE_EVENTS_KEY)) {
+            mLifecycleEventsTV.setText(savedInstanceState.getString(LIFECYCLE_EVENTS_KEY));
+        }
+
         logAndDisplayLifecycleEvent("onCreate");
     }
 
     private void logAndDisplayLifecycleEvent(String lifecycleEvent) {
         Log.d(TAG, "Lifecycle Event: " + lifecycleEvent);
         mLifecycleEventsTV.append(lifecycleEvent + "\n");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        logAndDisplayLifecycleEvent("onSaveInstanceState");
+        String lifecycleEventsText = mLifecycleEventsTV.getText().toString();
+        outState.putString(LIFECYCLE_EVENTS_KEY, lifecycleEventsText);
     }
 
     @Override
